83fbf09156bce23388e840a92a394b2e70648d41
Steve Vissault <s.vissault@yahoo.fr>
2019-11-18 22:32:47 -0500

    day 2 done

--- intro/_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for dir in $(ls -d cours*); do
-    cd $dir
-    rm -rf ./pres/.cache
-    cat *.Rmd > ./pres/index.Rmd
-    Rscript -e "slidify::slidify('./pres/index.Rmd')"
-    cd ..
-done
